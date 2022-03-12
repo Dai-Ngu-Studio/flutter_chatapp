23beafa97a867b68fb6cf344f9f5157bd4186bc2
Nguyễn Dũng <dpron12345@gmail.com>
2022-03-12 16:11:53 +0700

    Add splash screen

---
+++ lib/constants.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF6b3aee);
+const kSecondaryColor = Color(0xFFf6f5fb);
+const kBackgroundColor = Color(0xFFF2F3F8);
+const kAnimationDuration = Duration(milliseconds: 200);
+
+const MaterialColor chatappColor = MaterialColor(
+  0xFF6b3aee,
+  <int, Color>{
+    50: Color(0xFFdaccff),
+    100: Color(0xFFcab7ff),
+    200: Color(0xFFb79dff),
+    300: Color(0xFF9771ff),
+    400: Color(0xFF7e51f9),
+    500: Color(0xFF6b3aee),
+    600: Color(0xFF5a25e9),
+    700: Color(0xFF4d15e1),
+    800: Color(0xFF450dd7),
+    900: Color(0xFF3b06c7),
+  },
+);
