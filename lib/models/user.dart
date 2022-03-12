6238da89026117a4c045df0b24b5a73c9f879d84
Nguyễn Dũng <dpron12345@gmail.com>
2022-03-12 22:46:14 +0700

    add models

---
+++ lib/models/user.dart
@@ -0,0 +1,9 @@
+class User {
+  final String? displayName;
+  final String? email;
+  final String? photoURL;
+  final String? provider;
+  final String? uid;
+
+  User({this.uid, this.email, this.displayName, this.provider, this.photoURL});
+}
