8aadc26d8ad6aae0e03d7cb9080311ad9b928062
Nguyễn Dũng <dpron12345@gmail.com>
2022-03-13 13:43:18 +0700

    rename to UserMenu -> UserSettingMenu

--- lib/views/screens/user_setting/user_setting_body.dart
+++ lib/views/screens/user_setting/user_setting_body.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chatapp/constants.dart';
 import 'package:flutter_chatapp/services/firebase/auth_service.dart';
-import 'package:flutter_chatapp/views/screens/user_setting/user_menu.dart';
+import 'package:flutter_chatapp/views/screens/user_setting/user_setting_menu.dart';
 
 class UserSettingBody extends StatelessWidget {
   const UserSettingBody({Key? key}) : super(key: key);
@@ -14,7 +14,7 @@ class UserSettingBody extends StatelessWidget {
       padding: kPadding,
       child: Column(
         children: [
-          UserMenu(
+          UserSettingMenu(
             text: "Sign out",
             onPressed: () => _googleSignIn.signOut(context: context),
           )
