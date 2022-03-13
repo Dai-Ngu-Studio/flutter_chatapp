4f31e57ce60cab98285f23ba80adea4e0a13885a
Nguyễn Dũng <dpron12345@gmail.com>
2022-03-14 01:54:13 +0700

    update show user avatar

--- lib/views/screens/home/components/user_setting_button.dart
+++ lib/views/screens/home/components/user_setting_button.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:flutter/material.dart';
+import 'package:flutter_chatapp/utils/utilities.dart';
 import 'package:flutter_chatapp/views/screens/chat_room/components/group_avatar/members.dart';
 import 'package:flutter_chatapp/views/screens/chat_room/components/group_avatar/users.dart';
 import 'package:flutter_chatapp/views/screens/user_setting/user_setting_screen.dart';
@@ -8,6 +10,8 @@ class UserSettingButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var firebaseUser = auth.FirebaseAuth.instance.currentUser;
+
     return Padding(
       padding: const EdgeInsets.only(top: 8, left: 8),
       child: InkWell(
@@ -22,10 +26,16 @@ class UserSettingButton extends StatelessWidget {
           backgroundColor: Colors.grey.shade400,
           members: [
             User(
-              firstName: "Nguyễn",
-              lastName: "Dũng",
-              imageUrl:
-                  "https://scontent.fsgn2-3.fna.fbcdn.net/v/t1.6435-9/133357830_1237341779995351_21528659595782306_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=5jXsS5eIDlYAX-x2Rk3&_nc_ht=scontent.fsgn2-3.fna&oh=00_AT_QVe9ewOR3kKBTnI7_Uddq84J1Qe_leIcBSx6mfWb56g&oe=6250503E",
+              firstName: Utilities.getBackgroundWhenNotLoadImage(
+                firebaseUser!.displayName ?? '',
+              ).substring(0, 1),
+              lastName: Utilities.getBackgroundWhenNotLoadImage(
+                firebaseUser.displayName ?? '',
+              ).substring(1, 2),
+              imageUrl: firebaseUser.photoURL ??
+                  Utilities.getBackgroundWhenNotLoadImage(
+                    firebaseUser.displayName!,
+                  ),
             ),
           ],
         ),
