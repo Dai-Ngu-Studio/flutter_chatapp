0b7db9231619b11134f0cdbf56186cf45596ea13
Nguyễn Dũng <dpron12345@gmail.com>
2022-03-14 23:49:54 +0700

    Add hero animation

--- lib/views/screens/home/components/user_setting_button.dart
+++ lib/views/screens/home/components/user_setting_button.dart
@@ -3,9 +3,9 @@ import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:flutter/material.dart';
 import 'package:flutter_chatapp/services/firebase/firestore_service.dart';
 import 'package:flutter_chatapp/utils/utilities.dart';
-import 'package:flutter_chatapp/views/screens/chat_room/components/group_avatar/members.dart';
-import 'package:flutter_chatapp/views/screens/chat_room/components/group_avatar/users.dart';
+import 'package:flutter_chatapp/views/screens/user_setting/user_setting_body.dart';
 import 'package:flutter_chatapp/views/screens/user_setting/user_setting_screen.dart';
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 
 class UserSettingButton extends StatefulWidget {
   const UserSettingButton({Key? key}) : super(key: key);
@@ -25,44 +25,53 @@ class _UserSettingButtonState extends State<UserSettingButton> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 14, vertical: 2),
-      padding: const EdgeInsets.symmetric(vertical: 4),
-      child: InkWell(
-        onTap: () {
-          Navigator.of(context).pushNamed(UserSettingScreen.routeName);
-        },
-        splashColor: Colors.transparent,
-        highlightColor: Colors.transparent,
-        borderRadius: BorderRadius.circular(50),
-        child: StreamBuilder(
-          stream: db
-              .getUserByID(auth.FirebaseAuth.instance.currentUser!.uid)
-              .asStream(),
-          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            return Container(
+    return StreamBuilder(
+      stream: db
+          .getUserByID(auth.FirebaseAuth.instance.currentUser!.uid)
+          .asStream(),
+      builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        return Container(
+          margin: const EdgeInsets.symmetric(horizontal: 14, vertical: 2),
+          padding: const EdgeInsets.symmetric(vertical: 4),
+          child: InkWell(
+            onTap: () {
+              Navigator.of(context).pushNamed(
+                UserSettingScreen.routeName,
+                arguments: UserSettingBodyArguments(
+                  user: types.User(
+                    id: snapshot.data?.docs[0]['uid'],
+                    firstName: snapshot.data?.docs[0]['displayName'],
+                    imageUrl: snapshot.data?.docs[0]['imageUrl'],
+                  ),
+                ),
+              );
+            },
+            child: Container(
               padding: const EdgeInsets.symmetric(horizontal: 2, vertical: 2),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(50),
               ),
-              child: CircleAvatar(
-                radius: 10,
-                backgroundColor: Colors.grey.shade400,
-                child: Text(
-                  Utilities.getBackgroundWhenNotLoadImage(
-                    snapshot.data?.docs[0]['displayName'].toString() ?? '',
+              child: Hero(
+                tag: 'user_setting_avatar',
+                child: CircleAvatar(
+                  radius: 10,
+                  backgroundColor: Colors.grey.shade400,
+                  child: Text(
+                    Utilities.getBackgroundWhenNotLoadImage(
+                      snapshot.data?.docs[0]['displayName'].toString() ?? '',
+                    ),
+                    style: const TextStyle(color: Colors.white),
                   ),
-                  style: const TextStyle(color: Colors.white),
-                ),
-                foregroundImage: NetworkImage(
-                  snapshot.data?.docs[0]['imageUrl'],
+                  foregroundImage: NetworkImage(
+                    snapshot.data?.docs[0]['imageUrl'],
+                  ),
                 ),
               ),
-            );
-          },
-        ),
-      ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
--- lib/views/screens/user_setting/user_setting_body.dart
+++ lib/views/screens/user_setting/user_setting_body.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chatapp/constants.dart';
 import 'package:flutter_chatapp/services/firebase/auth_service.dart';
+import 'package:flutter_chatapp/utils/utilities.dart';
 import 'package:flutter_chatapp/views/screens/user_setting/user_setting_menu.dart';
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 
 class UserSettingBody extends StatelessWidget {
   const UserSettingBody({Key? key}) : super(key: key);
@@ -9,11 +11,41 @@ class UserSettingBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final AuthService _googleSignIn = AuthService();
+    UserSettingBodyArguments args =
+        ModalRoute.of(context)!.settings.arguments as UserSettingBodyArguments;
 
     return Padding(
       padding: kPadding,
       child: Column(
         children: [
+          Column(
+            children: [
+              Hero(
+                tag: 'user_setting_avatar',
+                child: CircleAvatar(
+                  radius: 50,
+                  backgroundColor: Colors.grey.shade400,
+                  child: Text(
+                    Utilities.getBackgroundWhenNotLoadImage(
+                      args.user.firstName!,
+                    ),
+                    style: const TextStyle(color: Colors.white, fontSize: 40),
+                  ),
+                  foregroundImage: NetworkImage(args.user.imageUrl ?? ''),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                args.user.firstName!,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
           UserSettingMenu(
             text: "Sign out",
             onPressed: () => _googleSignIn.signOut(context: context),
@@ -22,4 +54,10 @@ class UserSettingBody extends StatelessWidget {
       ),
     );
   }
+}
+
+class UserSettingBodyArguments {
+  final types.User user;
+
+  UserSettingBodyArguments({required this.user});
 }
