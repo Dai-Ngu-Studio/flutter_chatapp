90d1b06c90481da2100c44cbba9a719ff13b8cbc
Nguyễn Dũng <dpron12345@gmail.com>
2022-03-14 22:51:52 +0700

    remove unneeded import

--- lib/services/firebase/firestore_service.dart
+++ lib/services/firebase/firestore_service.dart
@@ -1,8 +1,5 @@
-import 'dart:io';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_chatapp/utils/utilities.dart';
 
