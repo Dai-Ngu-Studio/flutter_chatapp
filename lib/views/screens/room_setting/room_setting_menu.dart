f2b670406532528c4320b549c073d8722d537430
Nguyễn Dũng <dpron12345@gmail.com>
2022-03-14 22:59:36 +0700

    add padding for room setting menu

--- lib/views/screens/room_setting/room_setting_menu.dart
+++ lib/views/screens/room_setting/room_setting_menu.dart
@@ -10,37 +10,41 @@ class RoomSettingMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: InkWell(
-        onTap: onPressed,
-        child: Container(
-          padding: kPadding,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            shape: BoxShape.rectangle,
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(.4),
-                offset: const Offset(0, 2),
-                blurRadius: 10,
-              ),
-            ],
-          ),
-          child: Row(
-            children: [
-              Expanded(
-                child: Text(
-                  text,
-                  style: const TextStyle(
-                    color: Colors.black,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w600,
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Material(
+        child: InkWell(
+          onTap: onPressed,
+          child: Container(
+            padding: kPadding,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(.4),
+                  offset: const Offset(0, 2),
+                  blurRadius: 10,
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    text,
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
                 ),
-              ),
-              const Icon(Icons.arrow_forward_ios, color: Colors.grey, size: 20),
-            ],
+                const Icon(Icons.arrow_forward_ios,
+                    color: Colors.grey, size: 20),
+              ],
+            ),
           ),
         ),
       ),
