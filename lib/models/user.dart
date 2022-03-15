267f6d9ba869d963105589e4d1a24ae736aa1287
Nguyễn Dũng <dpron12345@gmail.com>
2022-03-15 22:26:13 +0700

    remove models

--- lib/models/user.dart
+++
@@ -1,9 +0,0 @@
-class User {
-  final String? displayName;
-  final String? email;
-  final String? photoURL;
-  final String? provider;
-  final String? uid;
-
-  User({this.uid, this.email, this.displayName, this.provider, this.photoURL});
-}
